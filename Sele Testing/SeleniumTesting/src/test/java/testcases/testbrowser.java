455ec2dd55e10e69af11d64554111d7ffb6fe8be
kevin-morningstar <61743491+kevin-morningstar@users.noreply.github.com>
2020-03-18 23:21:08 +0530

    Add files via upload

--- Sele Testing/SeleniumTesting/src/test/java/testcases/testbrowser.java
+++ Sele Testing/SeleniumTesting/src/test/java/testcases/testbrowser.java
@@ -1,120 +1,93 @@
 package testcases;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class testbrowser {
+public class testbrowser<e> {
 
+	// declare the Webdriver and Wait as public
 	public WebDriver driver;
-
-	@BeforeSuite
-	public void beforeSuite() {
-		System.out.println("Before Suite will always execute prior to all annotations or tests in the suite.");
+	public WebDriverWait wait = new WebDriverWait(driver, 120);
+	
+	//The Constructor who loads all the elements from the object.properties
+	public testbrowser() throws IOException {
+		FileReader reader = new FileReader("browser\\object.properties");	//Access the properties file
+		Properties prop = new Properties(); //make the object of Properties
+		prop.load(reader); //access the properties file with object
 	}
 
+	//Method to access the Chrome browser
 	@BeforeTest
 	@Parameters({ "browser" })
-	public void setup(String browser) throws Exception {
-		if (browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "driver\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else if (browser.equalsIgnoreCase("chrome")) {
+	public void beforeclass(String browser) throws Exception {
+		System.out.println("Before Class");
+		if (browser.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
 			driver = new ChromeDriver();
-		} else if (browser.equalsIgnoreCase("Edge")) {
-			System.setProperty("webdriver.edge.driver", "driver\\MicrosoftWebDriver.exe");
-			driver = new EdgeDriver();
-		} else {
-			throw new Exception("Browser is not correct");
 		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
-
-	@BeforeClass
-	public void beforeClass(){
-		System.out.println("Before Class will always execute prior to Before Method and Test Method ");
 		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();		
+		driver.manage().window().maximize();
 	}
 
-	@BeforeMethod
-	@Parameters({ "usename", "password"})
-	public void beforeMethod(String uname, String pwd) throws IOException{
-		FileInputStream fis = new FileInputStream(new File("D://test1.xlsx"));
-		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		XSSFSheet sh = wb.getSheet("Sheet1");
-		XSSFRow row = sh.getRow(1);
-		uname = row.getCell(0).getStringCellValue();
-		pwd = row.getCell(1).getStringCellValue();
-		driver.get("https://www.flipkart.com");
-		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector("input._2zrpKA._1dBPDZ")).click();
-		driver.findElement(By.cssSelector("input._2zrpKA._1dBPDZ")).sendKeys(username);
-		driver.findElement(By.cssSelector("input._2zrpKA._3v41xv._1dBPDZ")).click();
-		driver.findElement(By.cssSelector("input._2zrpKA._3v41xv._1dBPDZ")).sendKeys(password);
-		driver.findElement(By.cssSelector("button._2AkmmA._1LctnI._7UHT_c")).click();
+	//Method to open flipkart.coma and login with valid credentials
+	@Test
+	@Parameters({ "username", "password" })
+	public void login(String username, String password, Properties prop) throws IOException {
+		System.out.println("Inside Test");
+		driver.get("https://www.flipkart.com"); //Access the flipakrt.com
+		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS); //Wait till flipkart.com loads
+		driver.findElement(By.xpath(prop.getProperty("userid"))).click();  //Click on User ID box to allow type the login id
+		driver.findElement(By.xpath(prop.getProperty("userid"))).sendKeys(username);	//Enter the login id
+		driver.findElement(By.xpath(prop.getProperty("userpassword"))).click();		 //Click on Password box to allow type the password
+		driver.findElement(By.xpath(prop.getProperty("userpassword"))).sendKeys(password);	//Enter the password
+		driver.findElement(By.xpath(prop.getProperty("login"))).click();		//Click on Login Button
 	}
 
-	@Test(priority = 1)
+	//Method to find product in search box and click on appropriate product
+	@Test(priority = 1, dependsOnMethods = { "login" })
 	@Parameters({ "product", "product_name", "price" })
-	public void testCase(String product, String product_name, int price) throws InterruptedException {
-		WebDriverWait wait = new WebDriverWait(driver, 120);
-		WebElement searchBox, productName;
-		String online_price;
-		int product_price;
-		Thread.sleep(1000);
-		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-		searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("LM6RPg")));
-		searchBox.sendKeys(product);
-		driver.findElement(By.className("vh79eN")).click();
-		productName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText(product_name)));
-		productName.click();
-		
-		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(tabs.get(1));
-		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-		
-		online_price = driver.findElement(By.cssSelector("div._1vC4OE._3qQ9m1")).getText();
-		product_price = Integer.parseInt((online_price.substring(1)));
-		System.out.println("Online Price: " + product_price);
-		System.out.println("My Price: " + price);
-		if (price <= product_price) {
+	public void find_product(String product, String product_name, Properties prop) throws InterruptedException {
+		Thread.sleep(1000); //Sleep for 1 Second
+		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS); // Page load time for 120 seconds
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty("searchBox")))).sendKeys(product); //Enter the product in search box
+		driver.findElement(By.xpath("searchButton")).click(); //Click on Search button to search product
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("product_name")))).click();	//In product list click on appropriate product
+		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());	//Array created to handle multiple tabs in chrome browser
+		driver.switchTo().window(tabs.get(1)); //Switch to next Tab in chrome browser
+		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);	// Page load time for 120 Seconds
+	}
+
+	@Test(priority = 2)
+	@Parameters({ "price" })
+	public void compare_price(int price, Properties prop) {
+		String online_price = driver.findElement(By.xpath(prop.getProperty("onlinePrice"))).getText(); //Storing flipkart price as text in a variable
+		int product_price = Integer.parseInt((online_price.substring(1))); //Removing rupee symbol and converting the price into integer
+		System.out.println("Online Price: " + product_price); // Printing Online price
+		System.out.println("My Price: " + price); // Printing my price
+		if (price <= product_price) { //Comparing the both price if my price is less or equal then click on buy else print "Price is Different"
 			System.out.println(product_price);
-			wait.until(
-					ExpectedConditions.elementToBeClickable(By.cssSelector("button._2AkmmA._2Npkh4._2kuvG8._7UHT_c")))
-					.click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty("buyNow")))).click(); //Click on Buy Now Button
 		} else {
 			System.out.println("Price is Diffrent");
 		}
-
 	}
 
-	@AfterClass
-	public void afterClass() throws InterruptedException {
+	@AfterTest
+	public void afterTest() {
+		System.out.println("Closing Driver");
 		driver.quit();
 	}
 }
